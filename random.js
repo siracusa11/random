37e560bc65d66a2835f349292076ced81fc640da
siracusa11 <lopezalvaro006@gmail.com>
2015-04-24 15:49:31 +0200

    random version2

--- random.js
+++ random.js
@@ -1,7 +1,9 @@
 var numero = Math.random();
 
-if(numero <= 0.5){
-	console.log('\n' + numero + ' MENOR que 0,5 \n');
-} else {
-	console.log('\n' + numero + ' MAYOR que 0,5 \n');
+ivar str =' MAYOR que 0,5';
+
+if (numero <= 0.5){
+	str = ' MENOR que 0,5';
 }
+
+console.log('\n' + numero + str + '\n');
